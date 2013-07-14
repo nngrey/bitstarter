abcc2348ce0e947b3c4bc7ebd23a1cc8c5e0ac3e
Nathan Grey <greynng@gmail.com>
2013-07-14 05:19:34 +0000

    lost

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var buf_contents = fs.readFileSync('index.html','utf-8');
+var str_contents = buf_contents.toString();
 app.get('/', function(request, response) {
-  fs.readFileSync("./index.html").toString();
+  response.send(str_contents);
 });
 
 var port = process.env.PORT || 5000;
