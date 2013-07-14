83ce0f368ae1bb0a678896f069c02e63c4381bd8
Nathan Grey <greynng@gmail.com>
2013-07-14 04:14:31 +0000

    Modified web.js by adding ind buffer

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(index.html)));
+  var ind = new Buffer(fs.readFileSync(index,html); 
+  response.send(buf.toString(ind));
 });
 
 var port = process.env.PORT || 5000;
