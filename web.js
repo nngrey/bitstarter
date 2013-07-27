297154e301d914004a364f9a2f5560ab5269baf4
Nathan Grey <greynng@gmail.com>
2013-07-27 02:33:57 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
-var buf_contents = fs.readFileSync('index.html','utf-8');
-var str_contents = buf_contents.toString();
+
 app.get('/', function(request, response) {
-  response.send(str_contents);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
